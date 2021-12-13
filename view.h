54d4e024e7f35180e12bf240c1e9056f41f72517
jemai sameh <64665747+jemai-sameh@users.noreply.github.com>
2021-12-13 18:36:03 +0100

    Create view.h

---
+++ view.h
@@ -0,0 +1,10 @@
+#ifndef view_h
+
+void inputName(Joueur *joueur);
+void askLevel(char *level);
+void printGrille(int rowIndex,int colIndex);
+void display();
+void rules();
+
+#define view_h
+#endif
